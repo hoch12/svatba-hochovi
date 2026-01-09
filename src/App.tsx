096d1ce031d35e0577d97232e79e8677f3a5442e
hoch12 <161817511+hoch12@users.noreply.github.com>
2026-01-09 11:51:34 +0100

    Fix router basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// ZMĚNA: Importujeme HashRouter místo BrowserRouter
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -10,18 +11,20 @@ const queryClient = new QueryClient();
 
 const App = () => (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        {/* TADY JE TA ZMĚNA: Přidal jsem basename s názvem tvého repozitáře */}
-        <BrowserRouter basename="/eli-ka-kry-tof-wedding">
-          <Routes>
-            <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
+        <TooltipProvider>
+            <Toaster />
+            <Sonner />
+
+            {/* ZMĚNA: HashRouter funguje na GitHub Pages vždycky a nepotřebuje basename */}
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </HashRouter>
+
+        </TooltipProvider>
     </QueryClientProvider>
 );
 
